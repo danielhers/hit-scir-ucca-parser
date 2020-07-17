64d5146c433a345e1b2a390a5c8bab732c2f7c8c
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2020-07-17 09:33:52 +0200

    Training script for DIKU cluster

--- bash/train.sh
+++ bash/train.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 #SBATCH --mem=30G
 #SBATCH --time=5-0
-#SBATCH -p gpu --gres=gpu:titanx:1
+#SBATCH -p gpu --gres=gpu:titanrtx:1
 #SBATCH --array=1-3%1
 
-# UCCA
+exp=$1
+CHECKPOINTS=checkpoints/$exp${PREFIX:-}$SLURM_ARRAY_TASK_ID
+rm -rf $CHECKPOINTS
+
 CUDA_VISIBLE_DEVICES=0 \
+CUDA_DEVICE=0 \
 TRAIN_PATH=data/ewt.train.aug.streusle.mrp \
 DEV_PATH=data/ewt.dev.aug.streusle.mrp \
-BERT_PATH=bert/wwm_cased_L-24_H-1024_A-16 \
-WORD_DIM=1024 \
+BERT_PATH=/science/image/nlp-letre/bert/wwm_cased_L-24_H-1024_A-16/ \
 LOWER_CASE=FALSE \
 SEED=$RANDOM \
 BATCH_SIZE=8 \
+EPOCHS=50 \
 allennlp train \
--s checkpoints/ucca_bert${PREFIX:-}$SLURM_ARRAY_TASK_ID \
+-s $CHECKPOINTS \
 --include-package utils \
 --include-package modules \
 --include-package metrics \
 --file-friendly-logging \
-config/bert.jsonnet "$@"
+config/$exp.jsonnet
